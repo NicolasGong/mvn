d8a1a8c72929a80899f723363034da71d8b61e71
nicolasgong <nicolasgongyn@gmail.com>
2018-06-24 20:49:22 +0800

    增加部分代码

--- demo/src/main/java/com/gyn/network/NettyClientHandler.java
+++ demo/src/main/java/com/gyn/network/NettyClientHandler.java
@@ -1,5 +1,7 @@
 package com.gyn.network;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
@@ -7,14 +9,17 @@ public class NettyClientHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        super.channelActive(ctx);
-        ctx.writeAndFlush("hello!!!".getBytes());
+        System.out.println("client channel active!!");
+        ByteBuf buf = Unpooled.buffer();
+        buf.writeBytes("hello!!!".getBytes());
+        ctx.writeAndFlush(buf).sync();
+        System.out.println("client channel active end!!");
     }
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-        super.exceptionCaught(ctx, cause);
         System.out.println("client handler error!!");
+        cause.printStackTrace();
         ctx.close();
     }
 }
--- demo/src/main/java/com/gyn/network/NettyServerHandler.java
+++ demo/src/main/java/com/gyn/network/NettyServerHandler.java
@@ -1,5 +1,6 @@
 package com.gyn.network;
 
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
@@ -8,8 +9,18 @@ public class NettyServerHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        super.channelActive(ctx);
         System.out.println("channel active !!!");
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("channel read !!!");
+        ByteBuf bytes = (ByteBuf)msg;
+        int length = bytes.readableBytes();
+        byte[] bytes1 = new byte[length];
+        bytes.readBytes(bytes1);
+        String str = new String(bytes1);
+        System.out.println(str);
     }
 
     @Override
